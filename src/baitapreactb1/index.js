8e08006a3d9ef67e6e6cbcf29e18add5968b8c72
daiquoctuan <anhtuan6923@gmail.com>
2022-12-15 13:35:57 +0700

    khoi tao

---
+++ src/baitapreactb1/index.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+
+class Baitapreact1 extends Component{
+render(){
+    return(
+        <div>Bài tập react b1</div>
+    )
+}
+}
+export default Baitapreact1;
